6661cfb25e9257ac7338cc84060919ce4b6fb85d
crewd <newspaper321@nate.com>
2023-01-10 19:22:10 +0900

    refactor: 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'cc32!@',
+      password: process.env.DB_PASSWORD,
       database: 'dev',
       entities: ['**/*.entity.ts'],
       synchronize: true,
